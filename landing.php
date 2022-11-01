1e04f73adb277bf8ac8a530a194c6dd877dcb377
jkapusc <91202313+jkapusc@users.noreply.github.com>
2022-11-01 15:55:22 -0400

    Adding section

--- landing.php
+++ landing.php
@@ -15,20 +15,33 @@
   <body style="background-color: #92B9BD ">
 	<!--Navagation-->
 	<?php include("navbar.php")?>
-	  
-  <div class="container-bg">
-	<hr class="dashed">
-<div class="jumbotron">
-    <h1 class="display-4">Hello, world!</h1>
-	  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-	  <hr class="my-4">
-	  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-	  <p class="lead"> <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> </p>
-	</div>
+	<!--First Section-->
+	<div class="container-bg">
+		<hr class="dashed">
+		<div class="row">
+			<div class="col-xl-6">
+		    <img src="img/carcopter.png" class="img-fluid" alt="carcopter"> </div>
+			<div class="col-xl-6">
+			  <div class="jumbotron">
+				  <h1 class="display-4">Car to Helicopter Convertion</h1>
+				  <p class="lead">Never before has it been this easy to take your car to the sky.</p>
+				  <div class="row">
+					<div class="col-xl-6">
+						<a class="btn btn-primary btn-lg" href="#" role="button">Buy Now</a>
+					</div>
+					<div class="col-xl-6">
+						<a class="btn btn-primary btn-lg" href="#" role="button">Watch Demo</a>
+					</div>
+			    </div>				  
+			  </div>
+            </div>
+	  </div>
 <hr class="dashed">
-  </div>
+	</div>
 	  
+	<!--Footer-->
 	<?php include("footer.php")?>
+	  
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 	<script src="js/jquery-3.4.1.min.js"></script>
 
