c30ced9320f489973e8910d0f7f1f1dbae706eed
jkapusc <91202313+jkapusc@users.noreply.github.com>
2022-11-01 15:57:43 -0400

    Includes landing
    Includes landing page via php so editing landing is easier

--- index.php
+++ index.php
@@ -1,43 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    	<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-    	<title>Car to Sky</title>
-    	<!-- Bootstrap -->
-		<link href="css/bootstrap-4.4.1.css" rel="stylesheet">
-		<!--Main CSS-->
-		<link href="css/main.css" rel="stylesheet">
-	  	<!--Javascript-->
-	  	<script src="js/membershipForm.js"></script>
-	</head>
-  <body style="background-color: #92B9BD ">
-	<!--Navigation-->
-	<?php include("navbar.php")?>
-	  
-  <div class="container-bg">
-	<div class="banner">
-		
-	</div>
-<div class="jumbotron">
-    <h1 class="display-4">Hello, world!</h1>
-	  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-	  <hr class="my-4">
-	  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-	  <p class="lead"> <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> </p>
-	</div>
-<hr class="dashed">
-  </div>
-	  
-	<?php include("footer.php")?>
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-	<script src="js/jquery-3.4.1.min.js"></script>
-
-	<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="js/popper.min.js"></script> 
-	<script src="js/bootstrap-4.4.1.js"></script>
-	
-
+    <?php include("landing.php")?>
   </body>
 </html>
